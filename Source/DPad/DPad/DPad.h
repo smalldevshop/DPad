2c7176bd20f127860f89c48ea0ef742ac258ab2c
mattthousand <matt.buckley@raizlabs.com>
2015-12-25 13:02:30 -0500

    Create empty framework, create demo project, link local framework to demo project.

---
+++ Source/DPad/DPad/DPad.h
@@ -0,0 +1,19 @@
+//
+//  DPad.h
+//  DPad
+//
+//  Created by Matthew Buckley on 12/25/15.
+//  Copyright © 2015 Matt Buckley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DPad.
+FOUNDATION_EXPORT double DPadVersionNumber;
+
+//! Project version string for DPad.
+FOUNDATION_EXPORT const unsigned char DPadVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DPad/PublicHeader.h>
+
+
