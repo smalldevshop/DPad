772d2b3a600427c035ea3c2fb4cad2ad2120447b
mattthousand <matt.buckley@raizlabs.com>
2015-12-25 13:44:55 -0500

    Add SwiftLint to DPad, address a few SwiftLint warnings.

---
+++ Source/DPad/DPad/DPad.swift
@@ -0,0 +1,155 @@
+//
+//  DPad.swift
+//  DPad
+//
+//  Created by Matthew Buckley on 12/25/15.
+//  Copyright © 2015 Matt Buckley. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SpriteKit
+
+protocol DPadDelegate {
+    func directionDidChange(direction: DPad.Direction)
+    func touchesEnded(forDpad dPad: DPad)
+}
+
+final public class DPad: SKNode {
+
+    var delegate: DPadDelegate?
+    var movementVector: Vec2 = Vec2(x: 0.0, y: 0.0)
+    var initialPosition: CGPoint?
+
+    public enum Direction {
+        case None
+        case Left
+        case Right
+        case Up
+        case Down
+    }
+
+    var supportedDirections: Set<Direction> = Set()
+
+    var backgroundTexture: SKTexture? {
+        didSet {
+            let textureSize = backgroundTexture?.size() ?? CGSizeZero
+            let backgroundDiameter = min(textureSize.width, textureSize.height)
+
+            backGround?.texture = backgroundTexture
+            backGround?.size = CGSizeMake(backgroundDiameter, backgroundDiameter)
+
+            size = backGround?.size ?? CGSizeZero
+        }
+    }
+
+    var size: CGSize = CGSizeZero
+
+    var backGround: SKSpriteNode?
+    private var dPad: SKSpriteNode?
+
+    public class func new(backgroundTexture: SKTexture,
+        supportedDirections: Set<Direction>) -> DPad {
+            let dPad = DPad()
+            dPad.backgroundTexture = backgroundTexture
+            dPad.supportedDirections = supportedDirections
+
+            return dPad
+    }
+
+    override init() {
+        super.init()
+
+        userInteractionEnabled = true
+        backGround = SKSpriteNode()
+        dPad = SKSpriteNode()
+        dPad?.zPosition += 0.01
+
+        guard let backGround = backGround,
+            dPad = dPad else {
+                print("Failed to initialize child nodes")
+                return
+        }
+
+        dPad.addChild(backGround)
+        addChild(dPad)
+    }
+
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    public override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        super.touchesMoved(touches, withEvent: event)
+
+        // Set direction
+        guard let background = backGround else {
+            print("Failed to set up background")
+            return
+        }
+
+        let current = touches.first!.locationInNode(background)
+
+        let vec = Vec2(x: current.x, y: current.y)
+
+        let constrainedVec = constrainedVector(vec, supportedDirections: supportedDirections)
+
+        dPad?.position.x += constrainedVec.point.x
+        dPad?.position.y += constrainedVec.point.y
+
+        // Only update direction if vector magnitude is sufficently large.
+        if constrainedVec.magnitude > 1.5 {
+            switch constrainedVec.direction(constrainedVec.angle) {
+            case .None:
+                print("None: \(constrainedVec.angle)")
+                delegate?.directionDidChange(.None)
+            case .Up:
+                print("up: \(constrainedVec.angle)")
+                delegate?.directionDidChange(.Up)
+            case .Down:
+                print("down: \(constrainedVec.angle)")
+                delegate?.directionDidChange(.Down)
+            case .Right:
+                print("right: \(constrainedVec.angle)")
+                delegate?.directionDidChange(.Right)
+            case .Left:
+                print("left: \(constrainedVec.angle)")
+                delegate?.directionDidChange(.Left)
+            }
+        }
+
+    }
+
+    public override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        super.touchesEnded(touches, withEvent: event)
+        delegate?.directionDidChange(.None)
+        delegate?.touchesEnded(forDpad: self)
+    }
+
+}
+
+private extension DPad {
+
+    /**
+     Contrains vector based on DPad's supported directions (horizontal, vertical, all).
+
+     - parameter vector:              a Vec2 instance.
+     - parameter supportedDirections: a set of Directions.
+
+     - returns: a Vec2 instance.
+     */
+    func constrainedVector(vector: Vec2, supportedDirections: Set<Direction>) -> Vec2 {
+        var vec = Vec2(x: 0.0, y: 0.0)
+        switch supportedDirections.intersect([.Up, .Down, .Left, .Right]) {
+        case [.Up, .Down]:
+            vec.y = vector.y
+        case [.Left, .Right]:
+            vec.x = vector.x
+        case [.Up, .Down, .Left, .Right]:
+            vec = vector
+        default:
+            break
+        }
+        return vec
+    }
+}
---
+++ Source/DPad/DPad/Vec2.swift
@@ -0,0 +1,99 @@
+//
+//  Vec2.swift
+//  DPad
+//
+//  Created by Matthew Buckley on 12/25/15.
+//  Copyright © 2015 Matt Buckley. All rights reserved.
+//
+
+import Foundation
+
+public struct Vec2 {
+
+    public var x: CGFloat = 0.0
+    public var y: CGFloat = 0.0
+
+    public init(x: CGFloat, y: CGFloat) {
+        self.x = x
+        self.y = y
+    }
+
+    /// Computed point from vector.
+    public var point: CGPoint {
+        return CGPointMake(x, y)
+    }
+
+    /// Computed length of vector.
+    public var length: CGFloat {
+        return sqrt(x * x + y * y)
+    }
+
+    /// Computed magnitude of vector
+    public var magnitude: CGFloat {
+        return hypot(x, y)
+    }
+
+    /**
+     Scales vactor by factor.
+
+     - parameter factor: factor by which to scale the vector.
+
+     - returns: a Vec2 scaled by the factor passed in.
+     */
+    public mutating func scale(byFactor factor: CGFloat) -> Vec2 {
+        x = x * factor
+        y = y * factor
+        return self
+    }
+
+    /// Computed angle of vector relative to x axis.
+    public var angle: CGFloat {
+        return atan2(self.y, self.x)
+    }
+
+    /**
+     Calculates cross product of 2 vectors.
+
+     - parameter vector1: a Vec2 instance.
+     - parameter vector2: a Vec2 instance.
+
+     - returns: a CGFloat representing the cross product of the 2 vectors.
+     */
+    public func crossProduct(vector1: Vec2, vector2: Vec2) -> CGFloat {
+        return (vector1.x * vector2.y - vector1.y * vector2.x)
+    }
+
+    /**
+     Calculates dot product of 2 vectors.
+
+     - parameter vector1: a Vec2 instance.
+     - parameter vector2: a Vec2 instance.
+
+     - returns: a CGFloat representing the dot product of the 2 vectors.
+     */
+    public func dotProduct(vector1: Vec2, vector2: Vec2) -> CGFloat {
+        return vector1.x * vector2.x + vector1.y * vector2.y
+    }
+
+}
+
+extension Vec2 {
+
+    func direction(vectorAngle: CGFloat) -> DPad.Direction {
+        var direction = DPad.Direction.None
+        switch vectorAngle {
+            case let a where fabs(a) < CGFloat(M_PI_4):
+                direction = .Right
+            case let a where a > CGFloat(M_PI_4) && a <= CGFloat(3 * M_PI_4):
+                direction = .Up
+            case let a where (a > 0.0 && a > CGFloat(3 * M_PI_4)) || (a < 0.0 && fabs(a) > CGFloat(3 * M_PI_4)):
+                direction = .Left
+            case let a where a < 0.0 && fabs(a) >= CGFloat(M_PI_4) && fabs(a) <= CGFloat(3 * M_PI_4):
+                direction = .Down
+            default:
+                break
+        }
+        return direction
+    }
+
+}
